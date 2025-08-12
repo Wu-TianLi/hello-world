e56f72c4e0c1c17dd5593361e9369251161d31f2
Wu-TianLi <140076563+Wu-TianLi@users.noreply.github.com>
2025-08-12 08:45:20 +0000

    添加文件

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("This is a simple C program.\n");
+    printf("Let's learn C programming together!\n");
+    printf("Goodbye!");
+    return 0;
+}
